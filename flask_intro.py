2a5c7c73c8305e65b42e5479c51849ccaff5d63b
Kenneth Woo <kennethwoo11@gmail.com>
2023-06-12 16:12:07 -0400

    ooooo

--- flask_intro.py
+++ flask_intro.py
@@ -1,37 +1,70 @@
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
-from flask import Flask, render_template
+from sqlite3 import Error
+
 app = Flask(__name__)
+app.config['DATABASE'] = 'track.db'
+
+def create_connection():
+    conn = None;
+    try:
+        conn = sqlite3.connect(app.config['DATABASE'])
+    except Error as e:
+        print(e)
+    return conn
 
 @app.route('/')
 def home():
-    # Connect to your database and fetch results
-    conn = sqlite3.connect('track.db')
-    c = conn.cursor()
-    c.execute('SELECT * FROM Results')
-    results = c.fetchall()
+    conn = create_connection()
+    with conn:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Results')
+        results = cur.fetchall()
 
-    #athletes list
-    c.execute('SELECT DISTINCT Athlete FROM Results')
-    athletes = c.fetchall()
+        #athletes list
+        cur.execute('SELECT DISTINCT Athlete FROM Results')
+        athletes = cur.fetchall()
 
-    conn.close()
-
-    # Pass results to your template
     return render_template('index.html', results=results, athletes=athletes)
 
 @app.route('/athlete/<name>')
 def athlete_profile(name):
-    conn = sqlite3.connect('track.db')
-    c = conn.cursor()
-
-    # Retrieve all results for this athlete
-    c.execute('SELECT * FROM Results WHERE Athlete = ?', (name,))
-    results = c.fetchall()
-
-    conn.close()
+    conn = create_connection()
+    with conn:
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM Results WHERE Athlete = ?', (name,))
+        results = cur.fetchall()
 
     return render_template('profile.html', results=results)
 
+@app.route('/insert', methods=['GET', 'POST'])
+def insert_result():
+    if request.method == 'POST':
+        conn = create_connection()
+        with conn:
+            cur = conn.cursor()
+            cur.execute('INSERT INTO Results (Date, Athlete, Meet, Event, Result, Team) VALUES (?, ?, ?, ?, ?, ?)',
+                        (request.form.get('date'), request.form.get('athlete'), request.form.get('meet'),
+                        request.form.get('event'), request.form.get('result'), request.form.get('team')))
+            conn.commit()
+
+        return redirect(url_for('home'))
+
+    return render_template('insert.html')
+
+@app.route('/delete', methods=['GET', 'POST'])
+def delete_result():
+    conn = create_connection()
+    with conn:
+        cur = conn.cursor()
+        if request.method == 'POST':
+            cur.execute('DELETE FROM Results WHERE Result_ID = ?', (request.form.get('result_id'),))
+            conn.commit()
+
+        cur.execute('SELECT * FROM Results ORDER BY Result_ID DESC LIMIT 20')
+        results = cur.fetchall()
+
+    return render_template('delete.html', results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- import_results_csv.py
+++ import_results_csv.py
@@ -1,5 +1,6 @@
 import sqlite3
 import csv
+from datetime import datetime
 
 # Connect to SQLite database
 conn = sqlite3.connect('track.db')
@@ -12,6 +13,15 @@ with open('results_csv.csv', 'r') as f:
 
     for row in reader:
         date, athlete, meet, event, result, team = row
+        print('processing:', date, athlete, meet, event, result, team)
+        for row in reader:
+            # convert the date from MM/DD/YYYY to YYYY-MM-DD
+            date = datetime.strptime(row[0], '%m/%d/%Y').strftime('%Y-%m-%d')
+            athlete = row[1]
+            meet = row[2]
+            event = row[3]
+            result = row[4]
+            team = row[5]
 
         # Check for duplicates
         c.execute('''
@@ -25,6 +35,8 @@ with open('results_csv.csv', 'r') as f:
                 INSERT INTO Results (Date, Athlete, Meet_Name, Event, Result, Team)
                 VALUES (?, ?, ?, ?, ?, ?)
             ''', (date, athlete, meet, event, result, team))
+        else:
+            print('Record already exists:', date, athlete, meet, event, result, team)
 
 # Commit the changes and close the connection
 conn.commit()
