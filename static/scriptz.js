dc85d42b95fe48657267fcd1f89b7832fbf99bd7
Kenneth Woo <kennethwoo11@gmail.com>
2023-07-08 15:25:17 -0400

    bari mode

---
+++ static/scriptz.js
@@ -0,0 +1,11 @@
+// Get the button element
+var btnChangeColor = document.getElementById("btnChangeColor");
+
+// Add click event listener to the button
+btnChangeColor.addEventListener("click", function() {
+  // Get the body element
+  var body = document.body;
+
+  // Toggle the 'bari-mode' class on the body element
+  body.classList.toggle("bari-mode");
+});
