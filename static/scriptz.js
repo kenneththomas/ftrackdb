0b6eba84bad3f369436420ac3e23e729c79f8dff
Kenneth Woo <kennethwoo11@gmail.com>
2024-03-12 21:13:51 -0400

    remove color change button thing

--- static/scriptz.js
+++ static/scriptz.js
@@ -1,15 +1,3 @@
-// Get the button element
-var btnChangeColor = document.getElementById("btnChangeColor");
-
-// Add click event listener to the button
-btnChangeColor.addEventListener("click", function() {
-  // Get the body element
-  var body = document.body;
-
-  // Toggle the 'bari-mode' class on the body element
-  body.classList.toggle("bari-mode");
-});
-
 document.addEventListener("DOMContentLoaded", function() {
   setTimeout(function() {
     document.querySelector(".index-container").style.opacity = 1;
